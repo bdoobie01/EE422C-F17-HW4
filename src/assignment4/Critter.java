316df573e98179edef3d2dcce090384ec68aa3e0
bdoobie01 <brian.dubb01@gmail.com>
2017-10-17 17:03:26 -0500

    Changes in Main and Critter

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -11,7 +11,6 @@ package assignment4;
  * Fall 2016
  */
 
-
 import java.lang.reflect.Constructor;
 import java.util.List;
 
@@ -20,105 +19,142 @@ import java.util.List;
  * no new public, protected or default-package code or data can be added to Critter
  */
 
-
 public abstract class Critter {
 	private static String myPackage;
-	private	static List<Critter> population = new java.util.ArrayList<Critter>();
+	private static List<Critter> population = new java.util.ArrayList<Critter>();
 	private static List<Critter> babies = new java.util.ArrayList<Critter>();
 
-	// Gets the package name.  This assumes that Critter and its subclasses are all in the same package.
+	// Gets the package name. This assumes that Critter and its subclasses are
+	// all in the same package.
 	static {
 		myPackage = Critter.class.getPackage().toString().split(" ")[1];
 	}
-	
+
 	private static java.util.Random rand = new java.util.Random();
+
 	public static int getRandomInt(int max) {
 		return rand.nextInt(max);
 	}
-	
+
 	public static void setSeed(long new_seed) {
 		rand = new java.util.Random(new_seed);
 	}
-	
-	
-	/* a one-character long string that visually depicts your critter in the ASCII interface */
-	public String toString() { return ""; }
-	
+
+	/*
+	 * a one-character long string that visually depicts your critter in the
+	 * ASCII interface
+	 */
+	public String toString() {
+		return "";
+	}
+
 	private int energy = 0;
-	protected int getEnergy() { return energy; }
-	
+
+	protected int getEnergy() {
+		return energy;
+	}
+
 	private int x_coord;
 	private int y_coord;
-	
+
 	protected final void walk(int direction) {
 	}
-	
+
 	protected final void run(int direction) {
-		
+
 	}
-	
+
 	protected final void reproduce(Critter offspring, int direction) {
-		//Made by Brian
-		
-		Class cn = offspring.getClass();
-		try{
-		Constructor constructor = cn.getConstructor();
-		Critter newBaby = (Critter) constructor.newInstance();
-		newBaby.energy = offspring.energy/2;
-		
-		babies.add(newBaby);		}
-		catch(Exception e){
+		// Made by Brian
+
+		Class<?> cn = offspring.getClass();
+		try {
+			Constructor<?> constructor = cn.getConstructor();
+			Critter newBaby = (Critter) constructor.newInstance();
+			newBaby.energy = offspring.energy / 2;
+
+			babies.add(newBaby);
+		} catch (Exception e) {
 			return;
 		}
 	}
 
 	public abstract void doTimeStep();
+
 	public abstract boolean fight(String oponent);
-	
+
 	/**
-	 * create and initialize a Critter subclass.
-	 * critter_class_name must be the unqualified name of a concrete subclass of Critter, if not,
-	 * an InvalidCritterException must be thrown.
-	 * (Java weirdness: Exception throwing does not work properly if the parameter has lower-case instead of
-	 * upper. For example, if craig is supplied instead of Craig, an error is thrown instead of
-	 * an Exception.)
+	 * create and initialize a Critter subclass. critter_class_name must be the
+	 * unqualified name of a concrete subclass of Critter, if not, an
+	 * InvalidCritterException must be thrown. (Java weirdness: Exception
+	 * throwing does not work properly if the parameter has lower-case instead
+	 * of upper. For example, if craig is supplied instead of Craig, an error is
+	 * thrown instead of an Exception.)
+	 * 
 	 * @param critter_class_name
 	 * @throws InvalidCritterException
 	 */
-	public static void makeCritter(String critter_class_name) throws InvalidCritterException {	
-		//Made by Brian & Turan Vural
-		try{
+	public static void makeCritter(String critter_class_name) throws InvalidCritterException {
+		// Made by Brian & Turan Vural
+		try {
+
+			Class<?> cc = Class.forName("assignment4." + critter_class_name);
+			Constructor<?> constructor = cc.getConstructor();
+			Critter newCrit = (Critter) constructor.newInstance();
+			newCrit.energy = Params.start_energy;
 			
-		Class cc = Class.forName(critter_class_name);
-		Constructor constructor = cc.getConstructor();
-		Critter newCrit = (Critter)constructor.newInstance();
-		newCrit.energy = Params.start_energy;
-		newCrit.population.add(newCrit); //add this critter to the population NOT DONE IN CONSTRUCTOR
-		
-		}
-		catch (Exception e){
+			Boolean empty = false;
+			while(!empty){
+
+				newCrit.x_coord = getRandomInt(Params.world_width);
+				newCrit.y_coord = getRandomInt(Params.world_height);
+				Boolean isEmpty = true;
+				for(Critter c : population){
+					if((c.x_coord == newCrit.x_coord) && (c.y_coord == newCrit.y_coord)){
+						isEmpty = false;
+						break;
+					}
+				}
+				
+				empty = isEmpty;
+			}
+			population.add(newCrit); // add this critter to the population NOT
+										// DONE IN CONSTRUCTOR
+
+		} catch (Exception e) {
 			throw new InvalidCritterException(critter_class_name);
 		}
-		
-		
-		
+
 	}
-	
+
 	/**
 	 * Gets a list of critters of a specific type.
-	 * @param critter_class_name What kind of Critter is to be listed.  Unqualified class name.
+	 * 
+	 * @param critter_class_name
+	 *            What kind of Critter is to be listed. Unqualified class name.
 	 * @return List of Critters.
 	 * @throws InvalidCritterException
 	 */
 	public static List<Critter> getInstances(String critter_class_name) throws InvalidCritterException {
 		List<Critter> result = new java.util.ArrayList<Critter>();
-	
+		try {
+			for (Critter c : population) {
+				Class<?> crit = Class.forName(critter_class_name);
+				if (c.getClass().equals(crit)) {
+					result.add(c);
+				}
+			}
+		} catch (Exception e) {
+			throw new InvalidCritterException(critter_class_name);
+		}
 		return result;
 	}
-	
+
 	/**
 	 * Prints out how many Critters of each type there are on the board.
-	 * @param critters List of Critters.
+	 * 
+	 * @param critters
+	 *            List of Critters.
 	 */
 	public static void runStats(List<Critter> critters) {
 		System.out.print("" + critters.size() + " critters as follows -- ");
@@ -127,7 +163,7 @@ public abstract class Critter {
 			String crit_string = crit.toString();
 			Integer old_count = critter_count.get(crit_string);
 			if (old_count == null) {
-				critter_count.put(crit_string,  1);
+				critter_count.put(crit_string, 1);
 			} else {
 				critter_count.put(crit_string, old_count.intValue() + 1);
 			}
@@ -137,55 +173,58 @@ public abstract class Critter {
 			System.out.print(prefix + s + ":" + critter_count.get(s));
 			prefix = ", ";
 		}
-		System.out.println();		
+		System.out.println();
 	}
-	
-	/* the TestCritter class allows some critters to "cheat". If you want to 
-	 * create tests of your Critter model, you can create subclasses of this class
-	 * and then use the setter functions contained here. 
+
+	/*
+	 * the TestCritter class allows some critters to "cheat". If you want to
+	 * create tests of your Critter model, you can create subclasses of this
+	 * class and then use the setter functions contained here.
 	 * 
-	 * NOTE: you must make sure that the setter functions work with your implementation
-	 * of Critter. That means, if you're recording the positions of your critters
-	 * using some sort of external grid or some other data structure in addition
-	 * to the x_coord and y_coord functions, then you MUST update these setter functions
-	 * so that they correctly update your grid/data structure.
+	 * NOTE: you must make sure that the setter functions work with your
+	 * implementation of Critter. That means, if you're recording the positions
+	 * of your critters using some sort of external grid or some other data
+	 * structure in addition to the x_coord and y_coord functions, then you MUST
+	 * update these setter functions so that they correctly update your
+	 * grid/data structure.
 	 */
 	static abstract class TestCritter extends Critter {
 		protected void setEnergy(int new_energy_value) {
 			super.energy = new_energy_value;
 		}
-		
+
 		protected void setX_coord(int new_x_coord) {
 			super.x_coord = new_x_coord;
 		}
-		
+
 		protected void setY_coord(int new_y_coord) {
 			super.y_coord = new_y_coord;
 		}
-		
+
 		protected int getX_coord() {
 			return super.x_coord;
 		}
-		
+
 		protected int getY_coord() {
 			return super.y_coord;
 		}
-		
 
 		/*
-		 * This method getPopulation has to be modified by you if you are not using the population
-		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.
+		 * This method getPopulation has to be modified by you if you are not
+		 * using the population ArrayList that has been provided in the starter
+		 * code. In any case, it has to be implemented for grading tests to
+		 * work.
 		 */
 		protected static List<Critter> getPopulation() {
 			return population;
 		}
-		
+
 		/*
-		 * This method getBabies has to be modified by you if you are not using the babies
-		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.  Babies should be added to the general population 
-		 * at either the beginning OR the end of every timestep.
+		 * This method getBabies has to be modified by you if you are not using
+		 * the babies ArrayList that has been provided in the starter code. In
+		 * any case, it has to be implemented for grading tests to work. Babies
+		 * should be added to the general population at either the beginning OR
+		 * the end of every timestep.
 		 */
 		protected static List<Critter> getBabies() {
 			return babies;
@@ -196,49 +235,50 @@ public abstract class Critter {
 	 * Clear the world of all critters, dead and alive
 	 */
 	public static void clearWorld() {
+		population.clear();
 		// Complete this method.
 		// TODO complete this method
 	}
-	
+
 	public static void worldTimeStep() {
 		// Complete this method.
 		// TODO Complete this method
 
-		//doTimeStep for each critter in population
+		// doTimeStep for each critter in population
 		for (Critter cc : population) {
 			cc.doTimeStep();
 		}
 	}
-	
+
 	public static void displayWorld() {
 		// Complete this method.
 		// Made by Brian
-		
+
 		System.out.print("+");
-		for(int i=0; i<Params.world_width; i++){
+		for (int i = 0; i < Params.world_width; i++) {
 			System.out.print("-");
 		}
 		System.out.println("+");
-		
-		for (int j = 0; j<Params.world_height; j++){
+
+		for (int j = 0; j < Params.world_height; j++) {
 			System.out.print("|");
-			for(int i = 0; i<Params.world_width; i++){
+			for (int i = 0; i < Params.world_width; i++) {
 				Boolean found = false;
-				for(int w = 0; w<population.size(); w++){
-					if(population.get(w).x_coord == i && population.get(w).y_coord == j && !found){
+				for (int w = 0; w < population.size(); w++) {
+					if (population.get(w).x_coord == i && population.get(w).y_coord == j && !found) {
 						System.out.print(population.get(w).toString());
 						found = true;
 					}
 				}
-				if(!found){
+				if (!found) {
 					System.out.print(" ");
 				}
 			}
 			System.out.println("|");
 		}
-		
+
 		System.out.print("+");
-		for(int i=0; i<Params.world_width; i++){
+		for (int i = 0; i < Params.world_width; i++) {
 			System.out.print("-");
 		}
 		System.out.println("+");
--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -13,7 +13,7 @@ package assignment4;
 
 import java.util.Scanner;
 import java.io.*;
-
+import java.lang.reflect.Method;
 
 /*
  * Usage: java <pkgname>.Main <input file> test
@@ -22,170 +22,189 @@ import java.io.*;
  */
 public class Main {
 
-    static Scanner kb;    // scanner connected to keyboard input, or input file
-    private static String inputFile;    // input file, used instead of keyboard input if specified
-    static ByteArrayOutputStream testOutputString;    // if test specified, holds all console output
-    private static String myPackage;    // package of Critter file.  Critter cannot be in default pkg.
-    private static boolean DEBUG = false; // Use it or not, as you wish!
-    static PrintStream old = System.out;    // if you want to restore output to console
+	static Scanner kb; // scanner connected to keyboard input, or input file
+	private static String inputFile; // input file, used instead of keyboard
+										// input if specified
+	static ByteArrayOutputStream testOutputString; // if test specified, holds
+													// all console output
+	private static String myPackage; // package of Critter file. Critter cannot
+										// be in default pkg.
+	private static boolean DEBUG = false; // Use it or not, as you wish!
+	static PrintStream old = System.out; // if you want to restore output to
+											// console
 
+	// Gets the package name. The usage assumes that Critter and its subclasses
+	// are all in the same package.
+	static {
+		myPackage = Critter.class.getPackage().toString().split(" ")[1];
+	}
 
-    // Gets the package name.  The usage assumes that Critter and its subclasses are all in the same package.
-    static {
-        myPackage = Critter.class.getPackage().toString().split(" ")[1];
-    }
+	/**
+	 * Main method.
+	 *
+	 * @param args
+	 *            args can be empty. If not empty, provide two parameters -- the
+	 *            first is a file name, and the second is test (for test output,
+	 *            where all output to be directed to a String), or nothing.
+	 */
+	public static void main(String[] args) {
+		if (args.length != 0) {
+			try {
+				inputFile = args[0];
+				kb = new Scanner(new File(inputFile));
+			} catch (FileNotFoundException e) {
+				System.out.println("USAGE: java Main OR java Main <input file> <test output>");
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				System.out.println("USAGE: java Main OR java Main <input file>  <test output>");
+			}
+			if (args.length >= 2) {
+				if (args[1].equals("test")) { // if the word "test" is the
+												// second argument to java
+					// Create a stream to hold the output
+					testOutputString = new ByteArrayOutputStream();
+					PrintStream ps = new PrintStream(testOutputString);
+					// Save the old System.out.
+					old = System.out;
+					// Tell Java to use the special stream; all console output
+					// will be redirected here from now
+					System.setOut(ps);
+				}
+			}
+		} else { // if no arguments to main
+			kb = new Scanner(System.in); // use keyboard and console
+		}
 
-    /**
-     * Main method.
-     *
-     * @param args args can be empty.  If not empty, provide two parameters -- the first is a file name,
-     *             and the second is test (for test output, where all output to be directed to a String), or nothing.
-     */
-    public static void main(String[] args) {
-        if (args.length != 0) {
-            try {
-                inputFile = args[0];
-                kb = new Scanner(new File(inputFile));
-            } catch (FileNotFoundException e) {
-                System.out.println("USAGE: java Main OR java Main <input file> <test output>");
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                System.out.println("USAGE: java Main OR java Main <input file>  <test output>");
-            }
-            if (args.length >= 2) {
-                if (args[1].equals("test")) { // if the word "test" is the second argument to java
-                    // Create a stream to hold the output
-                    testOutputString = new ByteArrayOutputStream();
-                    PrintStream ps = new PrintStream(testOutputString);
-                    // Save the old System.out.
-                    old = System.out;
-                    // Tell Java to use the special stream; all console output will be redirected here from now
-                    System.setOut(ps);
-                }
-            }
-        } else { // if no arguments to main
-            kb = new Scanner(System.in); // use keyboard and console
-        }
+		/* Do not alter the code above for your submission. */
+		/* Write your code below. */
+		// TODO complete main
 
-        /* Do not alter the code above for your submission. */
-        /* Write your code below. */
-        // TODO complete main
+		// Prompt the user for input
+		System.out.println("Welcome to Critters!");
 
-        //Prompt the user for input
-        System.out.println("Welcome to Critters!");
+		boolean playing = true;
+		while (playing) {
+			// commands are quit, show, step, seed, make, or stats
+			String command = kb.nextLine();
+			String[] commandParts = command.split("\\s+");
+			String commandRoot = null;
+			// TODO will this catch multiple bad inputs?
+			while (commandRoot == null) {
+				try {
+					commandRoot = getCommandRoot(commandParts[0]);
+				} catch (InvalidCommandException invalidCommand) {
+					System.out.println(invalidCommand);
+					command = kb.nextLine();
+				}
+			}
 
-        Critter critter = new Critter() {
-            @Override
-            public void doTimeStep() {
+			// decide which command was given
+			switch (commandRoot) {
+			// TODO does this "quit" statement play well with the others?
+			case "quit":
+				playing = false;
+				continue;
 
-            }
+			case "show":
+				// TODO consult with partner over implementation of this method
 
-            @Override
-            public boolean fight(String oponent) {
-                return false;
-            }
-        };
+				Critter.displayWorld();
+				break;
 
-        boolean playing = true;
-        while (playing) {
-            //commands are quit, show, step, seed, make, or stats
-            String command = kb.nextLine();
-            String [] commandParts = command.split("\\s+");
-            String commandRoot = null;
-            // TODO will this catch multiple bad inputs?
-            while (commandRoot.equals(null)) {
-                try {
-                    commandRoot = getCommandRoot(command);
-                } catch (InvalidCommandException invalidCommand) {
-                    System.out.println(invalidCommand);
-                    command = kb.nextLine();
-                }
-            }
+			case "step":
+				// TODO complete doTimeStep implementation
+				Critter.worldTimeStep();
+				break;
 
-            //decide which command was given
-            switch (commandRoot) {
-                // TODO does this "quit" statement play well with the others?
-                case "quit" :
-                    playing = false;
-                    continue;
+			case "seed":
+				try{
+					int num = Integer.parseInt(commandParts[1]);
+					Critter.setSeed(num);
+				}
+				catch(Exception e){}
+				break;
 
-                case "show" :
-                    // TODO consult with partner over implementation of this method
+			case "make":
+				try {
+					if (commandParts.length == 2) {
+						Critter.makeCritter(commandParts[1]);
+					} else {
+						int num = Integer.parseInt(commandParts[2]);
+						for (int i = 0; i < num; i++) {
+							Critter.makeCritter(commandParts[1]);
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("error processing " + command);					
+					System.out.println("ERROR UNACCOUNTED EXCEPTION");
+				}
 
+				break;
 
-                    critter.displayWorld();
-                    break;
+			case "stats":
+				try {
 
-                case "step" :
-                    //TODO complete doTimeStep implementation
-                    for (Critter c : critter.population) {
+					String requestedCritter = commandParts[1];
+					// Class c = Class.forName(requestedCritter);
+					// Critter newCrit = (Critter)
+					// c.getConstructor().newInstance();
 
-                    }
-                    break;
+					// Critter statsCritter;
+					Critter.runStats(Critter.getInstances(requestedCritter)); // how
+																				// to
+																				// pass
+																				// restricted
+																				// variables
+																				// into
+																				// these
+																				// methods?
 
-                case "seed" :
-                    break;
+				} catch (Exception e) {
+					if (e instanceof InvalidCommandException || e instanceof InvalidCritterException) {
+						System.out.println("error processing " + command);
+					}
+					System.out.println("ERROR UNACCOUNTED EXCEPTION");
+				}
+				break;
 
-                case "make" :
-                    break;
+			}
 
-                case "stats" :
-                    try {
+		}
 
-                        String requestedCritter = commandParts[1];
-                        Class c = Class.forName(requestedCritter);
-                        Critter newCrit = (Critter) c.getConstructor().newInstance();
+		// System.out.println("GLHF");
 
-                        Critter statsCritter;
-                        critter.runStats(); // how to pass restricted variables into these methods?
+		/* Write your code above */
+		System.out.flush();
 
-                    } catch (Exception e) {
-                        if (e instanceof InvalidCommandException || e instanceof InvalidCritterException) {
-                            System.out.println("error processing " + command);
-                        }
-                        System.out.println("ERROR UNACCOUNTED EXCEPTION");
-                    }
-                    break;
+	}
 
-            }
+	/**
+	 * This method returns the root command keyword from the command
+	 *
+	 * @return quit, show, step, seed, make, or stats
+	 * @author Turan Vural
+	 */
+	private static String getCommandRoot(String command) throws InvalidCommandException {
+		if (command.contains("quit")) {
+			return "quit";
+		}
+		if (command.contains("show")) {
+			return "show";
+		}
+		if (command.contains("step")) {
+			return "step";
+		}
+		if (command.contains("seed")) {
+			return "seed";
+		}
+		if (command.contains("make")) {
+			return "make";
+		}
+		if (command.contains("stats")) {
+			return "stats";
+		}
 
-        }
-
-        // System.out.println("GLHF");
-        
-        /* Write your code above */
-        System.out.flush();
-
-    }
-
-    /**
-     * This method returns the root command keyword from the command
-     *
-     * @return quit, show, step, seed, make, or stats
-     * @author Turan Vural
-     */
-    private static String getCommandRoot(String command) throws InvalidCommandException {
-        if (command.contains("quit")) {
-            return "quit";
-        }
-        if (command.contains("show")) {
-            return "show";
-        }
-        if (command.contains("step")) {
-            return "step";
-        }
-        if (command.contains("seed")) {
-            return "seed";
-        }
-        if (command.contains("make")) {
-            return "make";
-        }
-        if (command.contains("stats")) {
-            return "stats";
-        }
-
-        throw new InvalidCommandException(command);
-    }
-
+		throw new InvalidCommandException(command);
+	}
 
 }
